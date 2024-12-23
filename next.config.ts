87bd42c23ef664762b7ac5d02b6164f7bc341d52
SamTaen <samtaen@noreply.codeberg.org>
2024-12-23 20:40:59 +0100

    Actions: Set `NEXT_BASE_PATH` to correct base path for codeberg

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next"
 
+const envBasePath = process.env.NEXT_BASE_PATH
+
 const nextConfig: NextConfig = {
 	output: "export",
+	basePath: envBasePath,
 }
 
 export default nextConfig
