d7b2e2bf232242eaa41dcd7ef10727d62a97dce4
SamTaen <samtaen@noreply.codeberg.org>
2024-12-23 17:15:36 +0100

    Add Next.js, tailwind, eslint

---
+++ src/app/layout.tsx
@@ -0,0 +1,20 @@
+import { Metadata } from "next"
+import "./globals.css"
+
+export default function RootLayout({
+	children,
+}: Readonly<{
+	children: React.ReactNode
+}>) {
+	return (
+		<html lang="en">
+			<body>{children}</body>
+		</html>
+	)
+}
+
+export const metadata: Metadata = {
+	title: "Nextjs Template",
+	description: "Basic Nextjs template",
+	manifest: "/manifest.json",
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import type { FC } from "react"
+
+const Page: FC = () => (
+	<>
+		<h1 className="text-red-400">Hello World</h1>
+	</>
+)
+
+export default Page
