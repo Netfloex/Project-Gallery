89ca2bc08c11cbd2bf0ea6730f569fc185b06853
Guus van Meerveld <mail@guusvanmeerveld.dev>
2025-05-14 15:02:55 +0200

    Flake: use Prisma directly instead of the node packages version

--- flake.nix
+++ flake.nix
@@ -16,11 +16,12 @@
     in {
       devShells = {
         default = pkgs.mkShell {
-          packages = with pkgs; [nodejs yarn nodePackages.prisma openssl pkg-config];
+          packages = with pkgs; [nodejs yarn prisma openssl pkg-config];
 
           shellHook = with pkgs; ''
             export PRISMA_QUERY_ENGINE_BINARY="${prisma-engines}/bin/query-engine"
             export PRISMA_QUERY_ENGINE_LIBRARY="${prisma-engines}/lib/libquery_engine.node"
+            export PRISMA_SCHEMA_ENGINE_BINARY="${prisma-engines}/bin/schema-engine";
             export PRISMA_INTROSPECTION_ENGINE_BINARY="${prisma-engines}/bin/introspection-engine"
             export PRISMA_FMT_BINARY="${prisma-engines}/bin/prisma-fmt"
           '';
