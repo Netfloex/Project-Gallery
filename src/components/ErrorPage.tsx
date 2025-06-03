b4b674fc21e8fb2f728260fafe63629593b3e130
Guus van Meerveld <mail@guusvanmeerveld.dev>
2025-06-03 23:37:09 +0200

    ErrorPage: Add description and children

--- src/components/ErrorPage.tsx
+++ src/components/ErrorPage.tsx
@@ -1,16 +1,22 @@
 "use client"
 
-import { FC } from "react"
-
 import { Button } from "@heroui/react"
 
-const LoadingPage: FC<{ errorText: string }> = ({ errorText }) => (
+import { FCC } from "@typings/FCC"
+
+const ErrorPage: FCC<{ errorText: string; description?: string }> = ({
+	errorText,
+	description,
+	children,
+}) => (
 	<div className="flex flex-grow flex-col items-center justify-center gap-2">
-		<h1 className="text-2xl">{errorText}</h1>
+		<h1 className="text-3xl">{errorText}</h1>
+		{description && <h2 className="text-xl">{description}</h2>}
 		<Button color="primary" onPress={() => history.back()}>
 			Go back
 		</Button>
+		{children}
 	</div>
 )
 
-export default LoadingPage
+export default ErrorPage
