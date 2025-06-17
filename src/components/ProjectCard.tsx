76af4f6b3c54bf3fb86f48876db75a8e305c1a42
Netfloex <38650595+Netfloex@users.noreply.github.com>
2025-06-17 17:27:03 +0200

    ProjectCard: Handle pluralization of "Votes"

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -10,38 +10,44 @@ import { Divider } from "@heroui/divider"
 import { FCC } from "@typings/FCC"
 import { PublicProject } from "@typings/project"
 
-const ProjectCard: FCC<{ project: PublicProject }> = ({ project }) => (
-	<Card>
-		<CardHeader className="flex flex-row items-center">
-			<p className="line-clamp-2 grow-1 text-xl">{project.name}</p>
-			<ProfilePicture user={project.uploader} />
-		</CardHeader>
+const ProjectCard: FCC<{ project: PublicProject }> = ({ project }) => {
+	const voteCount = project._count.votes
+	const voteTextPlural = voteCount === 1 ? "vote" : "votes"
 
-		<Divider />
+	return (
+		<Card>
+			<CardHeader className="flex flex-row items-center">
+				<p className="line-clamp-2 grow-1 text-xl">{project.name}</p>
+				<ProfilePicture user={project.uploader} />
+			</CardHeader>
 
-		<CardBody className="gap-2">
-			{!project.approved && <Chip color="danger">Not approved</Chip>}
-			<p className="line-clamp-3">{project.description}</p>
-		</CardBody>
+			<Divider />
 
-		<Divider />
+			<CardBody className="gap-2">
+				{!project.approved && <Chip color="danger">Not approved</Chip>}
+				<p className="line-clamp-3">{project.description}</p>
+			</CardBody>
 
-		<CardFooter className="gap-2">
-			<div className="flex grow-1 flex-row items-center gap-4 text-sm">
-				<p>{format(project.createdAt)}</p>
+			<Divider />
 
-				<p>{project._count.votes} vote(s)</p>
-			</div>
-			<Button
-				as={Link}
-				color="primary"
-				href={`/projects/${project.id}`}
-				// showAnchorIcon
-			>
-				Visit
-			</Button>
-		</CardFooter>
-	</Card>
-)
+			<CardFooter className="gap-2">
+				<div className="flex grow-1 flex-row items-center gap-4 text-sm">
+					<p>{format(project.createdAt)}</p>
+
+					<p>
+						{voteCount} {voteTextPlural}
+					</p>
+				</div>
+				<Button
+					as={Link}
+					color="primary"
+					href={`/projects/${project.id}`}
+				>
+					Visit
+				</Button>
+			</CardFooter>
+		</Card>
+	)
+}
 
 export default ProjectCard
